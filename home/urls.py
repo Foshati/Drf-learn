6f8d4c2e0dfe01591101b2fe2134b090d03cdb32
Foshati <amirrezafoshati@gmail.com>
2024-07-05 15:42:34 +0330

    class based views and function based views in drf

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import Home
+from . import views
+
+# from .views import Home
+
 
 urlpatterns = [
-    path("", Home.as_view(), name="home"),
+    # path("", views.Home.as_view(), name="home"), # ?  class based views render file html in django
+    # path("", views.Home, name="home")  # ? function based views endpoint
+    # path("", views.Home.as_view(), name="home")  # ? class based views endpoint
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
-from django.views import View
+# ?  class based views  render file html in django
+
+# from django.shortcuts import render
+# from django.views import View
 
 
-class Home(View):
-    def get(self, request):
-        return render(request, "home.html")
+# class Home(View):
+#     def get(self, request):
+#         return render(request, "home.html")
+
+
+# ? function based views drf
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+
+
+# @api_view(["GET"])
+# def Home(request):
+#     return Response({"name": "sam-get"})
+
+
+# ?  class based views drf
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+
+
+# class Home(APIView):
+#     def get(self, request):
+#         return Response({"name": "sam-post"})
