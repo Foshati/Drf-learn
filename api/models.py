42ab39e6c345a78ae3d936022937163987dfbe27
Foshati <amirrezafoshati@gmail.com>
2024-07-19 18:26:48 +0330

    create api function based views

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.PositiveSmallIntegerField()
+    bio = models.TextField(max_length=120)
+    email = models.EmailField()
+
+    def __str__(self) -> str:
+        return self.name
--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from api.models import UserProfile
+from api.serializers import UserProfileSerializers
+
+
+@api_view(["GET"])
+def ListProfile(request):
+    data = UserProfile.objects.all()
+    data_serializers = UserProfileSerializers(data, many=True)
+    return Response(data_serializers.data)
