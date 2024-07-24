8ef1097695e62479d8714d3f39415f8756930394
Foshati <amirrezafoshati@gmail.com>
2024-07-24 13:29:05 +0330

    create api request method post

--- api/serializers.py
+++ api/serializers.py
@@ -6,3 +6,14 @@ class UserProfileSerializers(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = "__all__"
+
+
+class CreateUserProfileSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = (
+            "name",
+            "age",
+            "bio",
+            "email",
+        )
--- api/views.py
+++ api/views.py
@@ -3,11 +3,18 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from api.models import UserProfile
-from api.serializers import UserProfileSerializers
+from api.serializers import UserProfileSerializers, CreateUserProfileSerializers
 
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def ListProfile(request):
-    data = UserProfile.objects.all()
-    data_serializers = UserProfileSerializers(data, many=True)
-    return Response(data_serializers.data)
+    if request.method == "GET":
+        data = UserProfile.objects.all()
+        data_serializers = UserProfileSerializers(data, many=True)
+        return Response(data_serializers.data)
+    if request.method == "POST":
+        req_data = request.data
+        data_serializers = CreateUserProfileSerializers(data=req_data)
+        data_serializers.is_valid(raise_exception=True)
+        data_serializers.save()
+        return Response(data_serializers.data, status=201)
